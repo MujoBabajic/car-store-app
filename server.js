8bfa4f8620f2389e7033d9e42cf18581483e5595
Mujo <mbabajic.babajic56@gmail.com>
2023-12-03 17:32:07 +0100

    added initial server setup

---
+++ server.js
@@ -0,0 +1,56 @@
+const http = require("http");
+const fs = require("fs");
+const qs = require("querystring");
+
+const PORT = 3131;
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/" && req.method === "GET") {
+    fs.readFile("./index.html", (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Internal Server Error");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      }
+    });
+  } else if (req.url === "/styles.css" && req.method === "GET") {
+    fs.readFile("./styles.css", (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Internal Server Error");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/css" });
+        res.end(data);
+      }
+    });
+  } else if (req.method === "POST" && req.url === "/submit") {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+    req.on("end", () => {
+      const formData = qs.parse(body);
+
+      const carName = formData.name;
+      const price = formData.price;
+      const description = formData.description;
+
+      const newCar = {
+        carName,
+        price,
+        description,
+      };
+
+      console.log(newCar);
+
+      res.writeHead(302, { Location: "/" });
+      res.end();
+    });
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
