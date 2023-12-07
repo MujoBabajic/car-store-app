ec3882b0d063b13a03dbac16f5884d9783371ef3
Mujo <mbabajic.babajic56@gmail.com>
2023-12-07 13:53:24 +0100

    added car adding and car removing functionalities

--- server.js
+++ server.js
@@ -4,22 +4,26 @@ const qs = require("querystring");
 
 const PORT = 3131;
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   if (req.url === "/" && req.method === "GET") {
-    fs.readFile("./index.html", (err, data) => {
+    fs.readFile("./index.html", async (err, data) => {
       if (err) {
         res.writeHead(500, { "Content-Type": "text/plain" });
-        res.end("Internal Server Error");
+        res.end("Server Error");
       } else {
         res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
+        const carsData = await fs.promises.readFile("./cars.json", "utf-8");
+        const updatedFile = data
+          .toString()
+          .replace("{{inject_here}}", generateTableBody(JSON.parse(carsData)));
+        res.end(updatedFile);
       }
     });
   } else if (req.url === "/styles.css" && req.method === "GET") {
     fs.readFile("./styles.css", (err, data) => {
       if (err) {
         res.writeHead(500, { "Content-Type": "text/plain" });
-        res.end("Internal Server Error");
+        res.end("Server Error");
       } else {
         res.writeHead(200, { "Content-Type": "text/css" });
         res.end(data);
@@ -29,7 +33,7 @@ const server = http.createServer((req, res) => {
     fs.readFile("./script.js", (err, data) => {
       if (err) {
         res.writeHead(500, { "Content-Type": "text/plain" });
-        res.end("Internal Server Error");
+        res.end("Server Error");
       } else {
         res.writeHead(200, { "Content-Type": "text/javascript" });
         res.end(data);
@@ -40,24 +44,60 @@ const server = http.createServer((req, res) => {
     req.on("data", (chunk) => {
       body += chunk.toString();
     });
-    req.on("end", () => {
+    req.on("end", async () => {
       const formData = qs.parse(body);
 
-      const carName = formData.name;
+      const name = formData.name;
       const price = formData.price;
       const description = formData.description;
 
       const newCar = {
-        carName,
+        name,
         price,
         description,
-        action: "Remove",
       };
 
-      console.log(newCar);
+      try {
+        const data = await fs.promises.readFile("./cars.json", "utf-8");
+        console.log("Data reading success");
 
-      res.writeHead(302, { Location: "/" });
-      res.end();
+        const carsArray = JSON.parse(data);
+        carsArray.push(newCar);
+
+        await fs.promises.writeFile("./cars.json", JSON.stringify(carsArray));
+        console.log("Data writing success");
+
+        res.writeHead(302, { Location: "/" });
+        res.end();
+      } catch (err) {
+        console.log(err);
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Server Error");
+      }
+    });
+  } else if (req.method === "POST" && req.url === "/remove") {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+    req.on("end", async () => {
+      try {
+        const formData = qs.parse(body);
+        const carToRemove = formData.name;
+
+        const data = await fs.promises.readFile("./cars.json", "utf-8");
+        const carsArray = JSON.parse(data);
+
+        const updatedCars = carsArray.filter((car) => car.name !== carToRemove);
+
+        await fs.promises.writeFile("./cars.json", JSON.stringify(updatedCars));
+        res.writeHead(302, { Location: "/" });
+        res.end();
+      } catch (err) {
+        console.log(err);
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Server Error");
+      }
     });
   }
 });
@@ -65,3 +105,25 @@ const server = http.createServer((req, res) => {
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+function generateTableBody(data) {
+  const tableRows = data
+    .map((car) => {
+      return `<tr>
+                <td>${car.name}</td>
+                <td>${formatMoney(car.price)}</td>
+                <td>${car.description}</td>
+                <td><form action="/remove" method="POST">
+                <input type="hidden" name="name" value="${car.name}" />
+                <input type="submit" class="remove-button" value="Remove"/></form>
+                </td>
+            </tr>`;
+    })
+    .join("");
+
+  return tableRows;
+}
+
+function formatMoney(value) {
+  return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
